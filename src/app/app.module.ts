086e7800a653984da71e0243c10968d5ee6c05f8
ddehghani <ddehghani@icloud.com>
2022-04-06 20:09:07 -0400

    components

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,10 +3,24 @@ import { BrowserModule } from '@angular/platform-browser';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { HeaderComponent } from './header/header.component';
+import { FooterComponent } from './footer/footer.component';
+import { SideBarComponent } from './side-bar/side-bar.component';
+import { ContentComponent } from './content/content.component';
+import { FoodListComponent } from './food-list/food-list.component';
+import { TodayListComponent } from './today-list/today-list.component';
+import { PieChartComponent } from './pie-chart/pie-chart.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    HeaderComponent,
+    FooterComponent,
+    SideBarComponent,
+    ContentComponent,
+    FoodListComponent,
+    TodayListComponent,
+    PieChartComponent
   ],
   imports: [
     BrowserModule,
