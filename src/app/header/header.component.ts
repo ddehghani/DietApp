154e3f7b1b34d59365b433334a487a5fc5458fc5
ddehghani <ddehghani@icloud.com>
2022-04-06 21:08:13 -0400

    update

--- src/app/header/header.component.ts
+++ src/app/header/header.component.ts
@@ -1,14 +1,20 @@
 import { Component, OnInit } from '@angular/core';
+import {formatDate} from '@angular/common';
 
 @Component({
   selector: 'app-header',
   templateUrl: './header.component.html',
   styleUrls: ['./header.component.css']
 })
+
+
+
 export class HeaderComponent implements OnInit {
-
-  constructor() { }
-
+  //formatDate(new Date(), 'yyyy/MM/dd', 'en');
+  date = formatDate(new Date(), 'yyyy/MM/dd', 'en');
+  constructor() { 
+    
+  }
   ngOnInit(): void {
   }
 
